974b8b45d81599c07ad1d2f04f056ffe94c99176
Vanessa Hay <vanehay@gmail.com>
2026-07-22 21:11:55 +0000

    fix(entrypoint): make entrypoint.sh POSIX compliant for Alpine Linux

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,20 @@
 
 ASSETS_DIR=/app/dist/assets
 
-find $ASSETS_DIR -type f -name "*.js" -print0 | while IFS= read -r -d $'\0' file; do
-  echo "Processing $file ..."
-  # Replace placeholders with actual environment variable values
-  sed -i "s|__VITE_API_URL__|${VITE_API_URL}|g" "$file"
-  sed -i "s|__VITE_API_VERSION__|${VITE_API_VERSION}|g" "$file"
-  sed -i "s|__VITE_ADMIN_EMAIL__|${VITE_ADMIN_EMAIL}|g" "$file"
-  sed -i "s|__VITE_GOOGLE_PROJECT_ID__|${VITE_GOOGLE_PROJECT_ID}|g" "$file"
-  sed -i "s|__VITE_GOOGLE_CLIENT_ID__|${VITE_GOOGLE_CLIENT_ID}|g" "$file"
-  sed -i "s|__VITE_GOOGLE_REDIRECT_URI__|${VITE_GOOGLE_REDIRECT_URI}|g" "$file"
-done
+if [ -d "$ASSETS_DIR" ]; then
+  for file in "$ASSETS_DIR"/*.js; do
+    if [ -f "$file" ]; then
+      echo "Processing $file ..."
+      sed -i "s|__VITE_API_URL__|${VITE_API_URL}|g" "$file"
+      sed -i "s|__VITE_API_VERSION__|${VITE_API_VERSION}|g" "$file"
+      sed -i "s|__VITE_ADMIN_EMAIL__|${VITE_ADMIN_EMAIL}|g" "$file"
+      sed -i "s|__VITE_GOOGLE_PROJECT_ID__|${VITE_GOOGLE_PROJECT_ID}|g" "$file"
+      sed -i "s|__VITE_GOOGLE_CLIENT_ID__|${VITE_GOOGLE_CLIENT_ID}|g" "$file"
+      sed -i "s|__VITE_GOOGLE_REDIRECT_URI__|${VITE_GOOGLE_REDIRECT_URI}|g" "$file"
+    fi
+  done
+fi
 
-# Start the Nginx web server
+# Start the Node web server
 echo "env setup done and run npm"
 npm start
